ad7bc76fc3594a4efd17ab555ef859252c248366
Yura Boyko <>
2022-11-22 15:49:58 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@dz211101/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+  
+  int a;
+  std::cout << "Number:" << std::endl;
+  std::cin >> a;
+  if(a == 1) std::cout << "January";
+  else if(a == 2) std::cout << "February";
+  else if(a == 3) std::cout << "March";
+  else if(a == 4) std::cout << "April";
+  else if(a == 5) std::cout << "May";
+  else if(a == 6) std::cout << "June";
+  else if(a == 7) std::cout << "July";
+  else if(a == 8) std::cout << "August";
+  else if(a == 9) std::cout << "September";
+  else if(a == 10) std::cout << "October";
+  else if(a == 11) std::cout << "November";
+  else if(a == 12) std::cout << "December";
+  else std::cout << "Error";
+  
+  
+
+}
